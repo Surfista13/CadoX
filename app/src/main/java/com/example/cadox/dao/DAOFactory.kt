15e6af6a2640e5756d30f379805fe1463e5299a0
BGG44 <brunogar@laposte.net>
2023-03-07 17:00:56 +0100

    modification du Singleton

--- app/src/main/java/com/example/cadox/dao/DAOFactory.kt
+++ app/src/main/java/com/example/cadox/dao/DAOFactory.kt
@@ -3,7 +3,7 @@ package com.example.cadox.dao
 import com.example.cadox.dao.memory.ArticleDAOMemoryImpl
 
 abstract class DAOFactory {
-    companion object Factory{
+    object Factory{
         fun createArticleDAO(): ArticleDAO = ArticleDAOMemoryImpl();
     }
 }
--- app/src/main/java/com/example/cadox/repository/ArticleRepository.kt
+++ app/src/main/java/com/example/cadox/repository/ArticleRepository.kt
@@ -1,9 +1,11 @@
 package com.example.cadox.repository
 
+import com.example.cadox.bo.Article
 import com.example.cadox.dao.DAOFactory
 
 class ArticleRepository {
 
-    val instance = DAOFactory.createArticleDAO();
+    val instance = DAOFactory.Factory.createArticleDAO();
+    val liste: Article = instance.selectById(10);
 
 }
