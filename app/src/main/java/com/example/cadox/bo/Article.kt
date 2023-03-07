ab7abf248c591f979caefc6209f6bf2ca2afb85d
BGG44 <brunogar@laposte.net>
2023-03-07 16:48:02 +0100

    passage de lacc Article en data class

--- app/src/main/java/com/example/cadox/bo/Article.kt
+++ app/src/main/java/com/example/cadox/bo/Article.kt
@@ -2,9 +2,5 @@ package com.example.cadox.bo
 
 import java.time.LocalDate
 
-class Article (var id:Long,var intitule:String,var description:String,var prix:Double,var niveau:Byte,var url:String?=null,var achete:Boolean=false,var dateAchat:LocalDate?=null) {
-
-    override fun toString(): String {
-        return "Article(id=$id, intitule='$intitule', description='$description', prix=$prix, niveau=$niveau, url=$url, achete=$achete, dateAchat=$dateAchat)"
-    }
+data class Article (var id:Long,var intitule:String,var description:String,var prix:Double,var niveau:Byte,var url:String?=null,var achete:Boolean=false,var dateAchat:LocalDate?=null) {
 }
